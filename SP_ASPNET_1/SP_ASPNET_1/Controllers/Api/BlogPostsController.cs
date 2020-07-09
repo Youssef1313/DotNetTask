bf27049feb8656a532730ae1815091a735ef3544
Youssef Victor <youssefvictor00@gmail.com>
2020-07-09 19:39:38 +0200

    Implement Like API

---
+++ SP_ASPNET_1/SP_ASPNET_1/Controllers/Api/BlogPostsController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using SP_ASPNET_1.DbFiles.Contexts;
+using SP_ASPNET_1.Models;
+
+namespace SP_ASPNET_1.Controllers.Api
+{
+    public class BlogPostsController : ApiController
+    {
+        private readonly IceCreamBlogContext _dbContext = new IceCreamBlogContext();
+
+
+        // PUT: api/BlogPosts/5
+        [ResponseType(typeof(void))]
+        [HttpPut]
+        public async Task<IHttpActionResult> LikeBlogPost(int id)
+        {
+            BlogPost blogPost = await _dbContext.BlogPosts.FirstOrDefaultAsync(p => p.BlogPostID == id);
+            if (blogPost == null)
+            {
+                return NotFound();
+            }
+
+            blogPost.Likes++;
+            await _dbContext.SaveChangesAsync();
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _dbContext.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
